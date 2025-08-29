6719cd509437599f4f11f65d755b52e1e7216f3a
charles lamb <lambcsgit@gmail.com>
2025-08-29 13:02:16 +0200

    My first website

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import os
+import requests
+import pandas as pd
+import datetime
 
 '''
 # TaxiFareModel front
@@ -15,7 +19,30 @@ Either as with the title by just creating a string (or an f-string). Or as with 
 
 1. Let's ask for:
 - date and time
+
+'''
+
+
+d = st.date_input(
+    "What date would you like to be picked up",
+    datetime.date(2019, 7, 6))
+st.write('We are picking you up on:', d)
+
+t = st.time_input('What time would you like to be picked up', datetime.time(8, 45))
+
+st.write('at', t)
+
+'''
+
 - pickup longitude
+
+'''
+
+
+
+
+'''
+
 - pickup latitude
 - dropoff longitude
 - dropoff latitude
@@ -30,11 +57,28 @@ See ? No need to load a `model.joblib` file in this app, we do not even need to 
 🤔 How could we call our API ? Off course... The `requests` package 💡
 '''
 
+SERVICE_URL = os.environ.get("SERVICE_URL")
+
 url = SERVICE_URL
 
 if url == 'https://taxifare.lewagon.ai/predict':
 
     st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
+
+
+X_pred = dict(
+        pickup_datetime=[pd.Timestamp("2013-07-06 17:18:00", tz='UTC')],
+        pickup_longitude=[-73.950655],
+        pickup_latitude=[40.783282],
+        dropoff_longitude=[-73.984365],
+        dropoff_latitude=[40.769802],
+        passenger_count=[1]
+    )
+
+
+answer = requests.get("https://taxifare-174146437405.europe-west1.run.app/predict",X_pred)
+
+st.json(answer.json())
 
 '''
 
