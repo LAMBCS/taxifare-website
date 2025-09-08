3d0e1a13a6753ab3f5dfe897983b5b1e095a47b9
charles lamb <lambcsgit@gmail.com>
2025-09-08 12:36:09 +0200

    local

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ if img_file_buffer is not None:
       ### Make request to  API (stream=True to stream response as bytes)
             res = requests.post(url, files={'img': img_file_buffer.getvalue()})
             print(res.json())
-            res_dict = json.loads(res.json())
+            #res_dict = json.loads(res.json())
 
             if res.status_code == 200:
         ### Display the image returned by the API
